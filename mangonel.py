5e37781140eb24343757607da854b3d261cbd5ac
Og B. Maciel <ogmaciel@gnome.org>
2013-05-03 17:32:47 -0400

    Turning a bunch of methods into a class.

--- mangonel.py
+++ mangonel.py
@@ -4,84 +4,84 @@ import requests
 import sys
 import time
 
-"""
-api = "<SERVER>/katello/api"
 
-s = requests.Session()
-s.auth = ("admin", "admin")
-s.verify = False
+class api(object):
 
-r = s.get(api)
+    def __init__(self, host, username, password):
+        self.host = host
+        self.api = "%s/api" % self.host
+        self.session = requests.Session()
+        self.session.auth = (username, password)
+        self.session.verify=False
 
-if not r.status_code == 200: sys.exit(-1)
-"""
+    def get_org(self, org_name):
+        result = None
 
+        orgs = self.url_get("organizations")
 
-def get_org(org_name, session):
-    result = None
-    
-    orgs = url_get(session, "organizations")
+        if orgs:
+            org = [org for org in orgs if org['name'] == org_name]
 
-    if orgs:
-        org = [org for org in orgs if org['name'] == org_name]
+            if org:
+                result = org[0]
 
-        if org:
-            result = org[0]
+        return result
 
-    return result
+    def get_org_environments(self, org, env_name):
+        result = None
 
-def get_org_environments(org, env_name, session):
-    result = None
-    
-    envs = url_get(session, "organizations/%s/environments" % org['label'])
+        envs = self.url_get("organizations/%s/environments" % org['label'])
 
-    if envs:
-        env = [env for env in envs if env['name'] == env_name]
+        if envs:
+            env = [env for env in envs if env['name'] == env_name]
 
-        if env:
-            result = env[0]
+            if env:
+                result = env[0]
 
-    return result
+        return result
 
 
-def url_get(session, url, timing=True):
-    result = None
+    def add_system(self, org, env_name):
+        result = None
 
-    # Strip leading forward slashes
-    if url.startswith("/"): url = url[1:]
-    
-    r = session.get("%s/%s" % (api, url))
-    if r.status_code == 200:
-        result = r.json()
+        env = self.get_org_environments(org, env_name)
+        system = generate_system()
 
-    return result
+        system['organization_id'] = org['label']
+        system['environment_id'] = env['id']
 
+        result = self.url_post("systems", system)
 
-def url_post(session, url, payload, timing=True):
-    result = None
-
-    # Strip leading forward slashes
-    if url.startswith("/"): url = url[1:]
-
-    headers = {'content-type': 'application/json'}
-
-    r = session.post("%s/%s" % (api, url),
-                     data=json.dumps(payload),
-                     headers=headers)
-    if r.status_code == 200:
-        result = r.json()
-
-    return result
+        return result
 
 
-def add_system(org, env_name, session):
-    result = None
+    def url_get(self, url, timing=True):
+        result = None
 
-    env = get_org_environments(org, env_name, session)
-    sys_uuid = 
-    system = generate_system()
+        # Strip leading forward slashes
+        if url.startswith("/"): url = url[1:]
 
-    system['organization_id'] = org['label']
-    system['environment_id'] = env['id']
+        r = self.session.get("%s/%s" % (self.api, url))
+        if r.status_code == 200:
+            result = r.json()
 
-    r = url_post(session, "%s/systems" % api, system)
+        return result
+
+
+    def url_post(self, url, payload, timing=True):
+        result = None
+
+        # Strip leading forward slashes
+        if url.startswith("/"): url = url[1:]
+
+        headers = {'content-type': 'application/json'}
+
+        r = self.session.post("%s/%s" % (self.api, url),
+                         data=json.dumps(payload),
+                         headers=headers)
+
+        import epdb; epdb.st()
+        if r.status_code == 200:
+            result = r.json()
+
+        return result
