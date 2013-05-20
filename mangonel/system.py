f0dd6b682295ec88cdc909b92eb1fb1d16fb3128
Og B. Maciel <ogmaciel@gnome.org>
2013-05-20 07:30:21 -0400

    Added new methods to systems.

--- mangonel/system.py
+++ mangonel/system.py
@@ -16,12 +16,33 @@ except ImportError, e:
 class System():
     api = SystemAPI()
     
-    def create_system(self, name=None):
+    def create_system(self, name, org, env, ak=None, type='system',
+                      release=None, sla=None, facts=None, view_id=None, installed_products=None):
 
         if name is None:
             name = generate_name(8)
-            
-        label = "label-%s" % name
-        description = "Generated automatically."
 
-        return self.api.register(name, org, env, activationkey, cp_type, facts)
+        if facts is None:
+            facts = generate_facts(name)
+
+        return self.api.register(name, org['label'], env['id'], ak, type, release, sla, facts, view_id, installed_products)
+
+
+    def delete_system(self, system):
+        return self.api.unregister(system['uuid'])
+
+    
+    def checkin(self, system):
+        return self.api.checkin(system['uuid'])
+
+
+    def system(self, system_id):
+        return self.api.system(system_id)
+
+    
+    def update_packages(self, system, packages=None):
+
+        if packages is None:
+            continue
+
+        return self.api.update_packages(system['uuid'], packages)
