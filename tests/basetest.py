7e126f5395c6f3936fa5b1017db5de74af66335c
Og B. Maciel <ogmaciel@gnome.org>
2013-06-25 17:30:58 -0400

    Adding a bit more control over logging level.

--- tests/basetest.py
+++ tests/basetest.py
@@ -13,7 +13,9 @@ class BaseTest(unittest.TestCase):
         self.user = os.getenv('USERNAME', None)
         self.password = os.getenv('PASSWORD', None)
         self.port = os.getenv('PORT', None)
+        self.verbosity = int(os.getenv('VERBOSITY', 3))
 
         logging.config.fileConfig("logging.conf")
 
         self.logger = logging.getLogger("mangonel")
+        self.logger.setLevel(self.verbosity * 10)
