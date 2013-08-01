3abc8e7e204627e5eef1448862d80addd029522b
Og B. Maciel <ogmaciel@gnome.org>
2013-08-01 11:58:29 -0400

    Moved generic valid/invalid names generator to commons.py.

--- mangonel/common.py
+++ mangonel/common.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# vim: ts=4 sw=4 expandtab ai
+
 import json
 import logging
 import os
@@ -67,6 +71,30 @@ def generate_name(min=4, max=8):
     pool1 = string.ascii_lowercase + string.digits
 
     return "%s" % str().join(r.choice(pool1) for x in range(random.randint(min, max)))
+
+VALID_NAMES = [
+    generate_name(1,1),
+    generate_name(255),
+    "%s-%s" % (generate_name(4), generate_name(4)),
+    "%s.%s" % (generate_name(4), generate_name(4)),
+    u"նոր օգտվող-%s" % generate_name(2),
+    u"新用戶-%s" % generate_name(2),
+    u"नए उपयोगकर्ता-%s" % generate_name(2),
+    u"нового пользователя-%s" % generate_name(2),
+    u"uusi käyttäjä-%s" % generate_name(2),
+    u"νέος χρήστης-%s" % generate_name(2),
+    "foo@!#$^&*( ) %s" % generate_name(),
+    "<blink>%s</blink>" % generate_name(),
+    "bar+{}|\"?hi %s" % generate_name(),
+    ]
+
+
+INVALID_NAMES = [
+    " ",
+    generate_name(256),
+    " " + generate_name(),
+    generate_name() + " ",
+    ]
 
 def generate_ipaddr():
 
